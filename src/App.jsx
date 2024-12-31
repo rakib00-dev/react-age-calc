ddceba52fd3f40fa34d842870dd276e00bb2c09f
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-31 22:49:46 +0600

    updated all things and maybe finished the work

--- src/App.jsx
+++ src/App.jsx
@@ -37,15 +37,20 @@ function App() {
   // let inputMonth = 4;
   // let inputYear = 2012;
 
-  let date = new Date();
-  let currentDay = date.getDate();
-  let currentMonth = 1 + date.getMonth();
-  let currentYear = date.getFullYear();
+  let currentDay = currentDate.getDate();
+  let currentMonth = 1 + currentDate.getMonth();
+  let currentYear = currentDate.getFullYear();
   let month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   if (isLeapYear(inputYear)) {
     month[1] = 29; // Update February inputDay to 29 if it's a leap year
   }
+
+  let monthDaysSum = 0;
+  month.forEach((e) => {
+    monthDaysSum += e;
+  });
+  console.log(monthDaysSum);
 
   if (inputDay > currentDay) {
     currentDay = currentDay + month[currentMonth - 1];
@@ -76,6 +81,8 @@ function App() {
     console.log({ inputYear, inputMonth, inputDay });
   }
 
+  let finalDaysOfTotalYear = y * monthDaysSum;
+
   return (
     <div className="grid place-content-center mt-10 h-fit w-fit m-auto gap-2 text-xl ">
       <h1 className="text-2xl text-center font-bold mb-6 md:text-4xl">
@@ -97,10 +104,49 @@ function App() {
         {/* ////inputs\\\\ */}
 
         {/* Year card */}
-        <Card title={'year:'} value={''} />
+        <Card title={'year:'} value={isNaN(y) ? '' : y} />
 
         {/* Month card */}
-        <Card title={'Month:'} value={''} />
+        <Card title={'Month:'} value={isNaN(y) ? '' : y * 12 + m} />
+
+        {/* Days card */}
+        <Card title={'Days:'} value={isNaN(y) ? '' : finalDaysOfTotalYear} />
+
+        {/* Hours card */}
+        <Card
+          title={'Hours:'}
+          value={isNaN(y) ? '' : finalDaysOfTotalYear * 24}
+        />
+
+        {/* Minutes card */}
+        <Card
+          title={'Minutes:'}
+          value={isNaN(y) ? '' : finalDaysOfTotalYear * 24 * 60}
+        />
+
+        {/* Seconds card */}
+        <Card
+          title={'Seconds:'}
+          value={isNaN(y) ? '' : finalDaysOfTotalYear * 24 * 60 * 60}
+        />
+
+        {/* deciseconds card */}
+        <Card
+          title={'deciseconds:'}
+          value={isNaN(y) ? '' : finalDaysOfTotalYear * 24 * 60 * 60 * 10}
+        />
+
+        {/* centiseconds card */}
+        <Card
+          title={'centiseconds:'}
+          value={isNaN(y) ? '' : finalDaysOfTotalYear * 24 * 60 * 60 * 100}
+        />
+
+        {/* milliseconds card */}
+        <Card
+          title={'milliseconds:'}
+          value={isNaN(y) ? '' : finalDaysOfTotalYear * 24 * 60 * 60 * 100}
+        />
       </div>
     </div>
   );
