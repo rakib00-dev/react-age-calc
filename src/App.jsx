25e1aa1c196b860804bd11c4c2970383a635ffda
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-31 10:34:12 +0600

    added new theme, initialize daisyUI, Added card, pass props as a function etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { useState } from 'react';
 import Input from './components/Input';
 
 function App() {
+  const [inputValue, setInputValue] = useState(new Date());
+
+  function takeInputValue(e) {
+    setInputValue(e.target.value);
+    console.log(inputValue.split('-')[0]);
+  }
+
   return (
-    <div className="">
-      <Input />
+    <div className="grid place-content-center mt-10 h-fit w-fit m-auto gap-2 text-xl ">
+      <h1 className="text-2xl text-center font-bold mb-6 md:text-4xl">
+        Age Calculator
+      </h1>
+      <Input inputValue={inputValue} onchange={takeInputValue} />
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-xl font-bold mb-6 text-center p-4 md:text-3xl">
+          Your Age In:
+        </h2>
+        {/* inputs */}
+        <div className="card bg-base-100 w-96 border border-t-1 border-black/10 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Years:</h2>
+            <p>{}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,29 @@
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({ onchange, inputValue }) => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-1 md:flex-row">
+      <div>
+        <label htmlFor="year">Pick Month/Day/Year from here: </label>
+      </div>
+      <div className="flex flex-row">
+        <input
+          id="year"
+          type="date"
+          step="1"
+          value={inputValue}
+          onChange={onchange}
+          className="border rounded-r-sm text-center w-fit p-2 bg-transparent border-black rounded-xl"
+        />
+        <button
+          id="btn"
+          type="button"
+          className="btn border border-black rounded-r-2xl"
+          // onClick={onchange}
+        >
+          Calculate
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
