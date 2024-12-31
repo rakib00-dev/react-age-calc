e139fb3e5062cb1f7eb8622452d866563201fe6b
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-31 12:51:31 +0600

    updated the functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,46 @@ import { useState } from 'react';
 import Input from './components/Input';
 
 function App() {
-  const [inputValue, setInputValue] = useState(new Date());
+  let currentDate = new Date();
+  const [inputValue, setInputValue] = useState(
+    `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}`
+  );
+  const [years, setYears] = useState();
+  const [months, setMonths] = useState();
+  const [weeks, setweeks] = useState();
+  const [days, setDays] = useState();
+
+  let calcYears = currentDate.getFullYear() - years;
+  let calcMonths = currentDate.getMonth() + 1 - months;
+  let calcDays = currentDate.getDate() - days;
+
+  let mL = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
 
   function takeInputValue(e) {
     setInputValue(e.target.value);
-    console.log(inputValue.split('-')[0]);
+  }
+
+  function clickFuntion() {
+    console.log('ok');
+    setYears(() => inputValue.split('-')[0]);
+    setMonths(() => inputValue.split('-')[1]);
+    console.log(months);
+
+    setDays(() => inputValue.split('-')[2]);
+    console.log({ years, months, days });
   }
 
   return (
@@ -14,16 +49,30 @@ function App() {
       <h1 className="text-2xl text-center font-bold mb-6 md:text-4xl">
         Age Calculator
       </h1>
-      <Input inputValue={inputValue} onchange={takeInputValue} />
+      <Input
+        inputValue={inputValue}
+        onchange={takeInputValue}
+        btnClicked={clickFuntion}
+      />
       <div className="flex flex-col justify-center items-center">
         <h2 className="text-xl font-bold mb-6 text-center p-4 md:text-3xl">
           Your Age In:
         </h2>
-        {/* inputs */}
-        <div className="card bg-base-100 w-96 border border-t-1 border-black/10 shadow-xl">
-          <div className="card-body">
+        {/* ////inputs\\\\ */}
+
+        {/* years card */}
+        <div className="card bg-base-100 w-96 border border-t-1 border-black/10 shadow-xl m-5">
+          <div className="card-body flex flex-row">
             <h2 className="card-title">Years:</h2>
-            <p>{}</p>
+            <p>{currentDate.getFullYear() - years} </p>
+          </div>
+        </div>
+
+        {/* months card */}
+        <div className="card bg-base-100 w-96 border border-t-1 border-black/10 shadow-xl m-5">
+          <div className="card-body flex flex-row">
+            <h2 className="card-title">Months:</h2>
+            <p>{Math.abs(calcMonths)} </p>
           </div>
         </div>
       </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,12 @@
-const Input = ({ onchange, inputValue }) => {
+const Input = ({ onchange, inputValue, btnClicked }) => {
   return (
     <div className="flex flex-col justify-center items-center gap-1 md:flex-row">
       <div>
-        <label htmlFor="year">Pick Month/Day/Year from here: </label>
+        <label htmlFor="date">Pick Month/Day/Year from here: </label>
       </div>
       <div className="flex flex-row">
         <input
-          id="year"
+          id="date"
           type="date"
           step="1"
           value={inputValue}
@@ -14,10 +14,9 @@ const Input = ({ onchange, inputValue }) => {
           className="border rounded-r-sm text-center w-fit p-2 bg-transparent border-black rounded-xl"
         />
         <button
-          id="btn"
           type="button"
           className="btn border border-black rounded-r-2xl"
-          // onClick={onchange}
+          onClick={btnClicked}
         >
           Calculate
         </button>
